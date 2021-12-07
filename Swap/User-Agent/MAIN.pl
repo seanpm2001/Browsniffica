648dd61083737f0e01f4f30c0d02db8b80a959d2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-06 18:30:07 -0800

    MAIN.pl (V1)
    Version 1 of the main script for this directory.

---
+++ Swap/User-Agent/MAIN.pl
@@ -0,0 +1,12 @@
+# Start of script
+# Under construction
+# This script is still under construction. It is not complete, far from ready, and likely not functional.
+
+user-agent use "Mozilla/5.0" with type "$browser" # Ok, this is absolute crap, I am going to stop here for now, I will come back when my knowledge of Perl is sufficient enough to do what I want to do here.
+
+""" File info
+File type: Perl source file (*.pl)
+File version: 1 (2021, Monday, December 6th at 6:30 pm)
+Line count (including blank lines and compiler line): 13
+"""
+# End of script
